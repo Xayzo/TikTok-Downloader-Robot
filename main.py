919bdf5c994edf9de078b2555f1d156c1d83b5b3
Xayzo <100010063+Xayzo@users.noreply.github.com>
2022-02-20 15:03:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,11 @@ app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=w
 
 @app.on_message(filters.command('start'))
 def start(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
+    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Support 🔰', url="https:/t.me/DionSupport")]]
     reply_markup = InlineKeyboardMarkup(kb)
     app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download TikTok video without Watermark.\n\n"
-                          "__**Developer :**__ __@JayBeeDev__\n"
-                          "__**Language :**__ __Python__\n"
-                          "__**Framework :**__ __🔥 Pyrogram__",
+                          "__**Developer :**__ __@Royzu__\n"
+                          "__**Language :**__ __Python__\n",
                      parse_mode='md',
                      reply_markup=reply_markup)
 
@@ -45,7 +44,7 @@ def start(client, message):
 
 @app.on_message(filters.command('help'))
 def help(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
+    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Support 🔰', url="https://t.me/DionSupport")]]
     reply_markup = InlineKeyboardMarkup(kb)
     app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download any TikTok video from a given link.\n\n"
                                             "__Send me a TikTok video link__",
@@ -116,7 +115,7 @@ def tiktok_dl(client, message):
                           document=f"./{directory}/{filename}",
                           caption=f"**File :** __{filename}__\n"
                           f"**Size :** __{total_size} MB__\n\n"
-                          f"__Uploaded by @{BOT_URL}__",
+                          "__Uploaded by @TikTok_Downloaderr_Robot__",
                           file_name=f"{directory}",
                           parse_mode='md',
                           progress=progress,
